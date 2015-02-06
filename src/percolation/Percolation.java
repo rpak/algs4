94a6818e0068dc2388f97b5f71d7d7d2626cbd40
Rob Pak <rob@carbonfive.com>
2015-02-06 06:13:48 +0000

    Check bounds for open and full

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,20 +1,27 @@
 public class Percolation {
+    private int[][] grid;
+
     // create N-by-N grid, with all sites blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
+
+        this.grid = new int[n][n];
     }
 
     // open site (row i, column j) if it is not open already
-    public void open(int i, int j) {
+    public void open(int i, int j) throws IndexOutOfBoundsException {
+        validateBounds(i, j);
     }
 
     // is site (row i, column j) open?
-    public boolean isOpen(int i, int j) {
+    public boolean isOpen(int i, int j) throws IndexOutOfBoundsException {
+        validateBounds(i, j);
         return false;
     }
 
     // is site (row i, column j) full?
-    public boolean isFull(int i, int j) {
+    public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
+        validateBounds(i, j);
         return false;
     }
 
@@ -25,5 +32,10 @@ public class Percolation {
 
     public static void main(String[] args) {
         System.out.println("Programming Assignment 1: Percolation");
+    }
+
+    private void validateBounds(int i, int j) throws IndexOutOfBoundsException {
+        if (i > this.grid.length || j > this.grid.length) throw new IndexOutOfBoundsException();
+        if (i <= 0 || j <= 0) throw new IndexOutOfBoundsException();
     }
 }
--- test/PercolationTest.java
+++ test/PercolationTest.java
@@ -1,11 +1,28 @@
-import org.hamcrest.core.CombinableMatcher;
 import org.junit.Test;
-import org.junit.Assert;
+import org.junit.*;
 
 public class PercolationTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorLessThanZero() {
         Percolation subject = new Percolation(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testOpenBounds() {
+        Percolation subject = new Percolation(20);
+        subject.open(21, 21);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsOpenBounds() {
+        Percolation subject = new Percolation(20);
+        subject.isOpen(21, 21);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsFullBounds() {
+        Percolation subject = new Percolation(20);
+        subject.isFull(21, 21);
     }
 }
