3c97c128a61ede63a0a640fe4c779b8959a278f5
Rob Pak <rob@carbonfive.com>
2015-02-06 20:08:26 +0000

    Percolation implementation

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,33 +1,126 @@
 public class Percolation {
-    private int[][] grid;
+    private int n;
+    private int siteCount;
+    private int[][] sites;
+    private WeightedQuickUnionUF connectedSites;
+    int virtualTopSiteId;
+    int virtualBottomSiteId;
 
-    // create N-by-N grid, with all sites blocked
+    // create N-by-N sites, with all sites blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
+        this.n = n;
+        this.siteCount = n * n;
+        this.sites = new int[n][n];
 
-        this.grid = new int[n][n];
+        this.connectedSites = new WeightedQuickUnionUF(this.siteCount);
+
+        this.virtualTopSiteId = n + 1;
+        this.virtualBottomSiteId = n + 2;
+
+        for (int i = 0; i < n; i++) {
+            this.connectedSites.union(virtualTopSiteId, i);
+            this.connectedSites.union(virtualBottomSiteId, (this.siteCount - 1 - i));
+            for (int j = 0; j < n; j++) {
+                this.sites[i][j] = 0;
+            }
+        }
+
+        System.out.println("Initialized " + (this.siteCount) + " sites.");
     }
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) throws IndexOutOfBoundsException {
         validateBounds(i, j);
+        int rowIndex = i - 1;
+        int columnIndex = j - 1;
+        int siteId = buildSiteId(rowIndex, columnIndex);
+
+        int topRowIndex = rowIndex - 1;
+        if (topRowIndex >= 0 && this.sites[topRowIndex][columnIndex] == 1) {
+            int topSiteId = buildSiteId(topRowIndex, columnIndex);
+            if (!connectedSites.connected(siteId, topSiteId)) {
+                connectedSites.union(siteId, topSiteId);
+            }
+
+            if (isFull(i - 1, j)) {
+                this.connectedSites.union(this.virtualTopSiteId, siteId);
+
+                if (i == n) {
+                    this.connectedSites.union(this.virtualBottomSiteId, this.virtualBottomSiteId);
+                }
+            }
+        }
+
+        int bottomRowIndex = rowIndex + 1;
+        if (bottomRowIndex < n && this.sites[bottomRowIndex][columnIndex] == 1) {
+            int bottomSiteId = buildSiteId(bottomRowIndex, columnIndex);
+            if (!connectedSites.connected(siteId, bottomSiteId)) {
+                connectedSites.union(siteId, bottomSiteId);
+            }
+
+            if (isFull(i + 1, j)) {
+                this.connectedSites.union(this.virtualTopSiteId, siteId);
+            }
+        }
+
+        int leftColumnIndex = columnIndex - 1;
+        if (leftColumnIndex >= 0 && this.sites[rowIndex][leftColumnIndex] == 1) {
+            int leftSiteId = buildSiteId(rowIndex, leftColumnIndex);
+            if (!connectedSites.connected(siteId, leftSiteId)) {
+                connectedSites.union(siteId, leftSiteId);
+            }
+
+            if (isFull(i, j - 1)) {
+                this.connectedSites.union(this.virtualTopSiteId, siteId);
+            }
+        }
+
+        int rightColumnIndex = columnIndex + 1;
+        if (rightColumnIndex < n && this.sites[rowIndex][rightColumnIndex] == 1) {
+            int rightSiteId = buildSiteId(rowIndex, rightColumnIndex);
+            if (!connectedSites.connected(siteId, rightSiteId)) {
+                connectedSites.union(siteId, rightSiteId);
+            }
+
+            if (isFull(i, j + 1)) {
+                this.connectedSites.union(this.virtualTopSiteId, siteId);
+            }
+        }
+
+        this.sites[rowIndex][columnIndex] = 1;
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) throws IndexOutOfBoundsException {
         validateBounds(i, j);
-        return false;
+        return this.sites[i - 1][j - 1] == 1;
     }
 
-    // is site (row i, column j) full?
+    /**
+     * A full site is an open site that can be connected to an open site in the top row via a chain of neighboring
+     * (left, right, up, down) open sites.
+     */
     public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
         validateBounds(i, j);
-        return false;
+        int rowIndex = i - 1;
+        int columnIndex = j - 1;
+        int siteId = buildSiteId(rowIndex, columnIndex);
+
+        if (isOpen(i, j) && this.connectedSites.connected(siteId, virtualTopSiteId)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    // does the system percolate?
+    /**
+     * We say the system percolates if there is a full site in the bottom row. In other words, a system percolates if
+     * we fill all open sites connected to the top row and that process fills some open site on the bottom row.
+     */
     public boolean percolates() {
-        return false;
+        return this.connectedSites.connected(virtualBottomSiteId, virtualTopSiteId);
     }
 
     public static void main(String[] args) {
@@ -35,7 +128,12 @@ public class Percolation {
     }
 
     private void validateBounds(int i, int j) throws IndexOutOfBoundsException {
-        if (i > this.grid.length || j > this.grid.length) throw new IndexOutOfBoundsException();
-        if (i <= 0 || j <= 0) throw new IndexOutOfBoundsException();
+        String message = "Coordinates, [" + i + "][" + j + "], out of bounds for N-by-N sites of " + this.sites.length;
+        if (i > this.sites.length || j > this.sites.length) throw new IndexOutOfBoundsException(message);
+        if (i <= 0 || j <= 0) throw new IndexOutOfBoundsException(message);
+    }
+
+    private int buildSiteId(int rowIndex, int columnIndex) {
+        return (rowIndex * this.n) + columnIndex;
     }
 }
--- test/PercolationTest.java
+++ test/PercolationTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
-import org.junit.*;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class PercolationTest {
 
@@ -24,5 +26,63 @@ public class PercolationTest {
     public void testIsFullBounds() {
         Percolation subject = new Percolation(20);
         subject.isFull(21, 21);
+    }
+
+    @Test
+    public void testOpen() {
+        Percolation subject = new Percolation(4);
+        subject.open(2, 3);
+        assertTrue(subject.isOpen(2, 3));
+        subject.open(3, 2);
+        assertTrue(subject.isOpen(3, 2));
+        subject.open(3, 4);
+        assertTrue(subject.isOpen(3, 4));
+        subject.open(4, 3);
+        assertTrue(subject.isOpen(4, 3));
+        subject.open(3, 3);
+        assertTrue(subject.isOpen(3, 3));
+    }
+
+    @Test
+    public void testIsFull() {
+        Percolation subject = new Percolation(4);
+        subject.open(1, 2);
+        assertTrue(subject.isFull(1, 2));
+        subject.open(2, 2);
+        assertTrue(subject.isFull(2, 2));
+        subject.open(3, 2);
+        assertTrue(subject.isFull(3, 2));
+        subject.open(4, 2);
+        assertTrue(subject.isFull(4, 2));
+    }
+
+    @Test
+    public void testIsNotFull() {
+        Percolation subject = new Percolation(4);
+        assertFalse(subject.isFull(1, 2));
+        assertFalse(subject.isFull(4, 4));
+    }
+
+    @Test
+    public void testPercolates() {
+        Percolation subject = new Percolation(4);
+        subject.open(1, 4);
+        subject.open(2, 2);
+        subject.open(2, 3);
+        subject.open(2, 4);
+        subject.open(3, 2);
+        subject.open(4, 1);
+        subject.open(4, 2);
+        assertTrue(subject.percolates());
+    }
+
+    @Test
+    public void testNotPercolates() {
+        Percolation subject = new Percolation(4);
+        subject.open(1, 2);
+        subject.open(2, 2);
+        subject.open(3, 2);
+        subject.open(4, 1);
+        assertFalse(subject.percolates());
     }
 }
