f4a6bfe2a304de85df608eb7657d7b4e01c46a87
Rob Pak <rob@carbonfive.com>
2015-02-06 01:29:57 +0000

    Add required interface for Percolation

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,5 +1,30 @@
 public class Percolation {
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+
+    }
+
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j) {
+
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        return false;
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        return false;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello worldz");
+        System.out.println("Programming Assignment 1: Percolation");
     }
 }
