3e7b49590328bf1dbc0c07c234629ec777481eb8
Rob Pak <rob@carbonfive.com>
2015-02-06 06:29:01 +0000

    Impound PercolationStats

---
+++ src/percolation/PercolationStats.java
@@ -0,0 +1,25 @@
+public class PercolationStats {
+    public PercolationStats(int n, int t) {
+
+    }
+
+    public double mean() {
+        return 0d;
+    }
+
+    public double stddev() {
+        return 0d;
+    }
+
+    public double confidenceLo() {
+        return 0d;
+    }
+
+    public double confidenceHi() {
+        return 0d;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
