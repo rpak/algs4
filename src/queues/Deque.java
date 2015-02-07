fbf75002afabaf51dc63e6e01ecea34bbad12c1e
Rob Pak <rob@carbonfive.com>
2015-02-07 06:46:15 +0000

    Impound queue assignment

---
+++ src/queues/Deque.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    // construct an empty deque
+    public Deque() {
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return -1;
+    }
+
+    // insert the item at the front
+    public void addFirst(Item item) {
+
+    }
+
+    // insert the item at the end
+    public void addLast(Item item) {
+
+    }
+
+    // delete and return the item at the front
+    public Item removeFirst() {
+        return null;
+    }
+
+    // delete and return the item at the end
+    public Item removeLast() {
+        return null;
+    }
+
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+
+    }
+}
