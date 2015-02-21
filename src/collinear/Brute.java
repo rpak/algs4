28cdb824f16649e5e60fd5857f9625db50ce2ff4
Rob Pak <rob@carbonfive.com>
2015-02-21 00:08:18 +0000

    Brute force implementation

--- src/collinear/Brute.java
+++ src/collinear/Brute.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Brute {
-    private static void foo(String dataFile) throws IOException {
+    private static Point[] buildPointsFromFile(String dataFile) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(dataFile));
         int n = Integer.parseInt(reader.readLine().trim());
         int actual = 0;
@@ -14,20 +15,59 @@ public class Brute {
             if (coordinates.length < 2) continue;
             int x = Integer.parseInt(coordinates[0]);
             int y = Integer.parseInt(coordinates[1]);
+            points[actual] = new Point(x, y);
             actual++;
         }
 
-        System.out.println("Brute Forcing " + n + " Points...");
-        System.out.println("Processed " + actual + " Points. DONE.");
+//        System.out.println("Brute forcing " + n + " points...");
+//        System.out.println("Finished reading " + actual + " points...");
         reader.close();
+
+        return points;
+    }
+
+    private static void findCollinearLines(Point[] points) {
+        Arrays.sort(points);
+        int cost = 0;
+
+        for (int i = 0; i < points.length; i++) {
+            points[i].draw();
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    for (int l = k + 1; l < points.length; l++) {
+                        double s1 = points[i].slopeTo(points[j]);
+                        double s2 = points[j].slopeTo(points[k]);
+                        double s3 = points[k].slopeTo(points[l]);
+
+                        cost++;
+                        if (s1 == s2 && s2 == s3) {
+                            points[i].drawTo(points[j]);
+                            points[j].drawTo(points[k]);
+                            points[k].drawTo(points[l]);
+                            System.out.println(points[i] + " -> " +
+                                               points[j] + " -> " +
+                                               points[k] + " -> " + points[l]);
+                        }
+                    }
+                }
+            }
+        }
+
+        // System.out.println("Finished finding collinear lines at a cost of " + cost);
     }
 
     public static void main(String[] args) {
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+
         try {
-            foo(args[0]);
+            Point[] points = buildPointsFromFile(args[0]);
+            findCollinearLines(points);
+
+            // while (true) { Thread.sleep(1000); }
         } catch (Exception e) {
             e.printStackTrace(System.err);
-            System.exit(1);
+            throw new RuntimeException(e.getMessage());
         }
     }
 }
--- test/collinear/BruteTest.java
+++ test/collinear/BruteTest.java
@@ -2,8 +2,31 @@ import org.junit.Test;
 
 public class BruteTest {
 
+    /**
+     * [0] (1234, 5678)
+     * [1] (14000, 10000)
+     * [2] (18000, 10000)
+     * [3] (19000, 10000)
+     * [4] (21000, 10000)
+     * [5] (32000, 10000)
+     */
     @Test
-    public void test() {
+    public void testInput6() {
         Brute.main(new String[] {"test/collinear/input6.txt"});
+    }
+
+    @Test
+    public void testInput8() {
+        Brute.main(new String[] {"test/collinear/input8.txt"});
+    }
+
+    @Test
+    public void testInput200() {
+        Brute.main(new String[] {"test/collinear/input200.txt"});
+    }
+
+    @Test
+    public void testInputRS1423() {
+        Brute.main(new String[] {"test/collinear/rs1423.txt"});
     }
 }
